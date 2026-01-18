dbeeafaf290fc2430b323c0cc5b6d9044e3fdec3
rzkir <rr8027896@gmail.com>
2026-01-18 12:03:36 +0700

    [update] add maps for contact

--- contact/index.php
+++ contact/index.php
@@ -131,8 +131,27 @@ $contacts = $controller->getAll();
         </div>
     </section>
 
+    <!-- Google Maps Section -->
+    <section class="container mx-auto py-4 px-4">
+        <div class="" data-aos="fade-up">
+            <h2 class="text-3xl font-bold text-slate-900 mb-6 text-center">Find Us on Map</h2>
+            <div class="rounded-2xl overflow-hidden shadow-lg">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.537340982221!2d106.822423!3d-6.192601!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f421229aaa1b%3A0x99d6f8a7abb690e2!2sGedung%20Perkantoran%20The%20Plaza!5e0!3m2!1sid!2sid!4v1768712326062!5m2!1sid!2sid"
+                    width="100%"
+                    height="450"
+                    style="border:0;"
+                    allowfullscreen=""
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"
+                    class="w-full">
+                </iframe>
+            </div>
+        </div>
+    </section>
+
     <!-- Contact List Section -->
-    <section class="bg-slate-50 py-20">
+    <section class="bg-slate-50 py-10">
         <div class="container mx-auto px-4 sm:px-6 lg:px-8">
             <div class="text-center max-w-3xl mx-auto mb-16" data-aos="fade-up">
                 <h2 class="text-3xl font-bold text-slate-900 mb-4">Connect With Us</h2>
